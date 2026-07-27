71426472e47e7c33c3e58328b01c79ee85737fc4
relaxrahulyt1-cloud <relaxrahulyt1@gmail.com>
2026-07-27 23:45:25 +0530

    Add page navigation and typewriter effect
    Implement page navigation and typewriter effect for a message.

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+const pages = document.querySelectorAll(".page");
+const nextBtns = document.querySelectorAll(".nextBtn");
+const openBtn = document.getElementById("openBtn");
+const typing = document.getElementById("typing");
 
+let currentPage = 0;
+
+const message = `Every friendship is a beautiful journey...
+Some people come into our lives and become unforgettable.
+
+Tanu, this little surprise is specially made for you. 💖`;
+
+function showPage(index){
+    pages.forEach(page => page.classList.remove("active"));
+    pages[index].classList.add("active");
+}
+
+function nextPage(){
+    currentPage++;
+
+    if(currentPage >= pages.length){
+        currentPage = pages.length-1;
+        return;
+    }
+
+    showPage(currentPage);
+}
+
+openBtn.addEventListener("click",()=>{
+    nextPage();
+    typeWriter();
+});
+
+nextBtns.forEach(btn=>{
+    btn.addEventListener("click",nextPage);
+});
+
+let i=0;
+
+function typeWriter(){
+
+typing.innerHTML="";
+
+const timer=setInterval(()=>{
+
+typing.innerHTML+=message.charAt(i);
+
+i++;
+
+if(i>=message.length){
+
+clearInterval(timer);
+
+}
+
+},35);
+
+}
